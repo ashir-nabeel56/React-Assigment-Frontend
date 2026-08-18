bb29e739f7bc9ffaba6b1d4f4b8e87e4997618a7
Nabeel <nabeelajmer@gmail.com>
2026-08-18 20:49:48 +0500

    hello

--- src/components/cate/CategoryPage.jsx
+++ src/components/cate/CategoryPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getProducts } from '../../api/backendapi';
-import './categoryPage.css'
+import './categorypage.css'
 function CategoryPage() {
     const { categoryName } = useParams();
     const [products, setProducts] = useState([]);
