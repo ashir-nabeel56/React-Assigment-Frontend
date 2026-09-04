4d3ef697790a8f83b25500bf4f126cbcdb65ec75
Nabeel <nabeelajmeri0@gmail.com>
2026-09-04 16:41:50 +0500

    something change

--- src/components/allcomponent.jsx/first.jsx
+++ src/components/allcomponent.jsx/first.jsx
@@ -4,13 +4,9 @@ import ProductList from "../pro/productsList"
 import Brands from "../secbrand/brand"
 import Topselling from "../tselling/topselling"
 import SlideBy from "../slide/slideBy"
-import Navbar from "../navbar/navbar"
-import TopBar from "../top/topbar"
 function first() {
   return (
     <div>
-      <TopBar/>
-      <Navbar/>
       <Hero/>
       <Brands/>
       <ProductList/>
